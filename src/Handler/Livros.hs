a669a44672aac93eab345217855a11acfc96ef2a
Allan Cruvinel <allan.cruvinel@gmail.com>
2021-05-24 22:02:01 -0300

    entrega 2

--- src/Handler/Livros.hs
+++ src/Handler/Livros.hs
@@ -11,6 +11,24 @@ import Text.Lucius
 
 getLivrosR :: Handler Html
 getLivrosR = do
+    livros <- runDB $ selectList [] [Asc LivrosNome]
     defaultLayout $ do
         toWidgetHead $(luciusFile "templates/livros-css.lucius")
         $(whamletFile "templates/livros.hamlet")
+
+getPerfilR :: LivrosId -> Handler Html 
+getPerfilR cid = do
+    livros <- runDB $ get404 cid
+    defaultLayout [whamlet|
+        <h1>
+            Pagina do livro #{livrosNome livros}
+        <h2>
+            Autor #{livrosAutor livros}   
+        <h2>
+            Categoria #{livrosCategoria livros}               
+    |]
+
+postApagarLivroR :: LivrosId -> Handler Html 
+postApagarLivroR cid = do
+    runDB $ delete cid
+    redirect LivrosR
--- src/Handler/Register.hs
+++ src/Handler/Register.hs
@@ -9,8 +9,30 @@ module Handler.Register where
 import Import
 import Text.Lucius
 
+formRegister :: Form Livros 
+formRegister = renderDivs $ Livros
+    <$> areq textField "Nome " Nothing 
+    <*> areq textField "Autor " Nothing 
+    <*> areq textField "Categoria " Nothing 
+    
+
 getRegisterR :: Handler Html
 getRegisterR = do
+    (widget,_) <- generateFormPost formRegister
+    msg <- getMessage 
     defaultLayout $ do
         toWidgetHead $(luciusFile "templates/register-css.lucius")
         $(whamletFile "templates/register.hamlet")
+
+postRegisterR :: Handler Html
+postRegisterR = do
+    ((result,_),_) <- runFormPost formRegister
+    case result of
+        FormSuccess livros -> do
+            runDB $ insert livros
+            setMessage [shamlet|
+                <div>
+                    LIVRO INSERIDO
+            |] 
+            redirect LivrosR 
+        _ -> redirect LivrosR
